2444522e95aeb7cdcf9d0f48d0bb456e4f829b9d
Matthew <Matthew@10.10.13.93>
2016-09-12 18:43:57 -0700

    changes to driver

--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,24 @@ public class Driver {
 		// Process command-line parameters to determine the directory to parse
 		// and output to produce.
 		// See the Execution section below for specifics.
+		// default file should be 'index.json'
+
+		for (int i = 0; i < args.length; i++) {
+
+			if (args[i].equalsIgnoreCase("-index")) {
+				if (args[i + 1] == null || !args[i + 1].toLowerCase().contains(".json")) {
+					System.out.println("default index.json created since no file provided");
+				} else {
+					System.out.println(args[i + 1]);
+				}
+				// maybe this is enough? i can technically still create the
+				// index if I construct it after
+			} else if (args[i].equalsIgnoreCase("-dir")) {
+				// check the next directory in args
+			}
+		}
 
 		System.out.println(Arrays.toString(args));
 	}
+
 }
