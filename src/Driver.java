a7d06c96c63842359b255e4255ea3b9f914cb0b8
Matthew <Matthew@10.10.13.93>
2016-09-13 18:09:54 -0700

    driver - working, traverseDirectory - in progress

--- src/Driver.java
+++ src/Driver.java
@@ -1,32 +1,25 @@
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Create code that handles parsing command-line arguments,
-		// so that you can easily retrieve the directory containing the text
-		// files to parse.
-		// Process command-line parameters to determine the directory to parse
-		// and output to produce.
-		// See the Execution section below for specifics.
-		// default file should be 'index.json'
-
 		for (int i = 0; i < args.length; i++) {
 
 			if (args[i].equalsIgnoreCase("-index")) {
-				
+
 				if (args[i + 1] == null || !args[i + 1].toLowerCase().contains(".json")) {
 					System.out.println("default 'index.json' created since no file provided");
+
 				} else if (args[i + 1].toLowerCase().contains(".json")) {
 					System.out.println(args[i + 1]);
 				}
-				
-				// maybe this is enough? i can technically still create the
-				// index if I construct it after elif case
-				
+
 			} else if (args[i].equalsIgnoreCase("-dir")) {
 				// check the next args for a directory
-				System.out.println(args[i+1]);
+
+				new traverseDirectory(args[i + 1]);
+				System.out.println("Path: " + Paths.get(args[i + 1]));
 			}
 		}
 
--- src/traverseDirectory.java
+++ src/traverseDirectory.java
@@ -1,13 +1,52 @@
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 // Create code that is able to traverse a directory and 
 // return a list of all the text files found within that directory.
 // Store the normalized relative path for file locations as a String object.
 // Do not convert the file paths to absolute paths!
 // Use the UTF-8 character encoding for all file processing, including reading and writing.
+
 public class traverseDirectory {
-	
-	public traverseDirectory(File inputFile) {
-		// TODO Auto-generated constructor stub
+	private static Path path;
+
+	public traverseDirectory() {
+	}
+
+	public traverseDirectory(String inputPath) {
+		path = Paths.get(inputPath);
+
+		try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
+
+			for (Path file : listing) {
+
+				// Check if this is a subdirectory
+				if (Files.isDirectory(file)) {
+
+					traverse(file);
+
+				} else if (file.endsWith(".txt")) {
+					// Add the file size next to the name
+					System.out.printf(file.toString());
+				}
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void traverse(Path directory) {
+
+		if (Files.isDirectory(directory)) {
+			traverse(directory);
+		} else if (directory.endsWith(".txt")) {
+			// save the text file location
+			System.out.println(directory.toString()); // path not printing
+		}
 	}
 }
